f2d6ee655a8fbee90381e05a4984ba1fec79d796
rafsan <rafsanamin18@gmail.com>
2020-10-11 10:18:34 +0600

    54.4 business info component

---
+++ client/src/components/Home/BusinessInfo/BusinessInfo.js
@@ -0,0 +1,40 @@
+import React from "react";
+import InfoCard from "../InfoCard/InfoCard";
+import {
+    faClock,
+    faPhone,
+    faMapMarker,
+} from "@fortawesome/free-solid-svg-icons";
+
+const infoData = [
+    {
+        title: "Opening Hours",
+        description: "We are open 24/7",
+        icon: faClock,
+        background: "primary",
+    },
+    {
+        title: "Visit our location",
+        description: "Brooklyn, Ny, 10036, United States",
+        icon: faMapMarker,
+        background: "dark",
+    },
+    {
+        title: "Contact us now",
+        description: "+102 356 8565425",
+        icon: faPhone,
+        background: "primary",
+    },
+];
+
+const BusinessInfo = () => {
+    return (
+        <section className="row">
+            {infoData.map((info) => (
+                <InfoCard info={info}></InfoCard>
+            ))}
+        </section>
+    );
+};
+
+export default BusinessInfo;
