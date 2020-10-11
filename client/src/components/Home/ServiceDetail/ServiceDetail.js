9f567b98b6d2558c71ced1da842fd3e8a1a94c40
rafsan <rafsanamin18@gmail.com>
2020-10-11 17:29:44 +0600

    54.6 services set up

---
+++ client/src/components/Home/ServiceDetail/ServiceDetail.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ServiceDetail = ({ service }) => {
+    return (
+        <div className="col-md-4 text-center">
+            <img src={service.img} style={{ height: "50px" }} alt="" />
+            <h5 className="mt-3 mb-3">{service.name}</h5>
+            <p className="text-secondary">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nesciunt officia similique, quam quibusdam iure perferendis!
+            </p>
+        </div>
+    );
+};
+
+export default ServiceDetail;
