eddcac71cdcf361fa6a09138a3e9271362d675ed
rafsan <rafsanamin18@gmail.com>
2020-10-11 21:10:38 +0600

    55.1 shared components extracted

--- client/src/components/Home/Home/Home.js
+++ client/src/components/Home/Home/Home.js
@@ -3,7 +3,7 @@ import Blogs from "../Blogs/Blogs";
 import Contact from "../Contact/Contact";
 import Doctors from "../Doctors/Doctors";
 import FeaturedService from "../FeaturedService/FeaturedService";
-import Footer from "../Footer/Footer";
+import Footer from "../../Shared/Footer/Footer";
 import Header from "../Header/Header";
 import MakeAppointment from "../MakeAppointment/MakeAppointment";
 import Services from "../Services/Services";
@@ -11,7 +11,7 @@ import Testimonials from "../Testimonials/Testimonials";
 
 const Home = () => {
     return (
-        <div>
+        <div style={{ overflow: "hidden" }}>
             <Header></Header>
             <Services></Services>
             <FeaturedService></FeaturedService>
