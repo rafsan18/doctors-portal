521a881b0c3eab25e728ecbc276dd346f41fd48c
rafsan <rafsanamin18@gmail.com>
2020-10-12 10:55:52 +0600

    55.4 react-modal and react-hook-form

--- client/src/components/Appointment/BookingCard/BookingCard.js
+++ client/src/components/Appointment/BookingCard/BookingCard.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import AppointmentForm from "../AppointmentForm/AppointmentForm";
 
 const BookingCard = ({ booking }) => {
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    const openModal = () => setIsOpen(true);
+    const closeModal = () => setIsOpen(false);
     return (
         <div className="col-md-4 mb-5">
             <div className="card p-3">
@@ -8,9 +13,17 @@ const BookingCard = ({ booking }) => {
                     <h5 className="card-title text-brand">{booking.subject}</h5>
                     <h6>{booking.visitingHour}</h6>
                     <p>{booking.totalSpace} Spaces Available</p>
-                    <button className="btn-brand text-uppercase">
+                    <button
+                        onClick={openModal}
+                        className="btn-brand text-uppercase"
+                    >
                         Book Appointment
                     </button>
+                    <AppointmentForm
+                        appointmentOn={booking.subject}
+                        modalIsOpen={modalIsOpen}
+                        closeModal={closeModal}
+                    ></AppointmentForm>
                 </div>
             </div>
         </div>
